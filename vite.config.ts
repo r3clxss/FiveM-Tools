fe5ff0912170a033239956870331766f009b0b3f
RecLxss <goldfistone@gmail.com>
2025-10-16 17:48:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hem/", 
+  base: "/fivem-tools/", 
   server: {
     host: "::",
     port: 8080,
